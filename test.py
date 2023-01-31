34ae69eac1ba01f89d8f8a5994b1f955694a3606
ixtel <igor.v.lukashin@gmail.com>
2023-01-31 15:44:10 +0600

    add test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+﻿from src import main
+
+def build(pkg_name='pkgbuilder', cmd=''):
+	main.build_package(pkg_name, rf'reqs release packlasts {cmd} packages_path=d:\python\packages')
+
+
+if __name__ == '__main__':
+	build(cmd='install')
